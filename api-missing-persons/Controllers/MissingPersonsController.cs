0ac048dcb3ed2512e64b5bc5706985fca142e0b6
William Xifaras <wxifaras@microsoft.com>
2024-09-18 11:08:34 -0400

    api-missing-persons

---
+++ api-missing-persons/Controllers/MissingPersonsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_missing_persons.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MissingPersonsController : ControllerBase
+    {
+        
+    }
+}
