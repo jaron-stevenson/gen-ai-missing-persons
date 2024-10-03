7d5348d7e4977cba2ec70d815e1cb0e292267947
Rickcau <rickcau@hotmail.com>
2024-10-03 19:16:36 -0400

    adding ui-frontend project for testing

---
+++ ui-frontend/Models/QuestionAnswer.cs
@@ -0,0 +1,30 @@
+﻿namespace ui_frontend.Models
+{
+    public class QuestionAnswer
+    {
+        public string? Question { get; set; }
+        public string? Answer { get; set; }
+
+        public bool? AnswerHidden { get; set; }
+
+        public List<Trip> Trips
+        {
+            get
+            {
+                return _trips;
+            }
+        }
+
+        private List<Trip> _trips { get; set; } = new List<Trip>();
+
+        public QuestionAnswer(string question, string answer)
+        {
+            Question = question;
+            Answer = answer;
+        }
+
+        public void AddTrip(Trip trip) => _trips.Add(trip);
+        public void AddTrips(List<Trip> trips) => _trips.AddRange(trips);
+
+    }
+}
